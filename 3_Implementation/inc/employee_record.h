b1f93829dbb600ce54216e77cf4fe8bda9235d73
murali980 <muralikrishna0980@gmail.com>
2021-04-15 11:34:48 +0530

    upload

---
+++ 3_Implementation/inc/employee_record.h
@@ -0,0 +1,23 @@
+#ifndef __employee_record_H__
+#define __employee_record_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in] operand1 
+* @return feild
+*/
+void add( void );
+void delete(void);
+void modify(void);
+void list(void);
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in] operand1 
+* @return print_minefield
+*/
+void exit( void );
+#endif  
